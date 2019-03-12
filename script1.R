28b9892bba1bb390bbaf590a34b61c7e0c9bb20d
Shafi Rubbani <shafirubbani@gmail.com>
2019-03-12 14:06:48 -0400

    Made geom_smooth plot

--- script1.R
+++ script1.R
@@ -1,7 +1,17 @@
 library(tidyverse)
-library(dplyr)
 library(gt)
-library(ggplot2)
+library(janitor)
 
-orig <- read_csv(file = "polls_EE-parliament_2019-03-12.csv")
+orig <- read_csv(file = "polls_EE-parliament_2019-03-12.csv") %>% 
+  clean_names()
 
+orig %>% 
+  ggplot(aes(date)) + 
+  geom_smooth(aes(y = ekre, colour = "Ekre")) + 
+  geom_smooth(aes(y = green, colour = "Green")) +
+  geom_smooth(aes(y = irl, colour = "I")) + 
+  geom_smooth(aes(y = kesk, colour = "Centew")) +
+  geom_smooth(aes(y = ref, colour = "Reform")) + 
+  geom_smooth(aes(y = sde, colour = "SDE")) +
+  geom_smooth(aes(y = eva, colour = "???")) + 
+  geom_smooth(aes(y = e200, colour = "E200"))
