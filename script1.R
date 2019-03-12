0b91094719285dc9641407161521130819429160
Shafi Rubbani <shafirubbani@gmail.com>
2019-03-12 14:44:33 -0400

    Gathered columns

--- script1.R
+++ script1.R
@@ -3,27 +3,17 @@ library(gt)
 library(janitor)
 
 orig <- read_csv(file = "https://pollofpolls.eu/get/polls/ES-parliament/format/csv") %>% 
-  clean_names()
+  clean_names() %>% 
+  gather(pp, psoe, bng, cc, erc, pnveaj, cs, vox, podemos, eh_bildu, pacma, p_de_cat, key = "party", value = "vote")
 
-orig %>% 
-  
-  #filter(date < as.Date("2018-03-12")) %>% 
-  
+good_firms <- orig %>% 
   group_by(firm) %>% 
-  
-  summarize(numbers = n()) %>% 
-  
-  ungroup() %>% 
-  
-  filter(Sys.Date() - date < 365, !is.na(firm), numbers >= 40) %>% 
-  
-  ggplot(aes(date)) + 
-  geom_smooth(aes(y = psoe, colour = "Ekre")) + 
-  geom_smooth(aes(y = BNG, colour = "Green")) +
-  geom_smooth(aes(y = cc, colour = "I")) + 
-  geom_smooth(aes(y = erc, colour = "Centew")) +
-  geom_smooth(aes(y = pnveaj, colour = "Reform")) 
-  #geom_smooth(aes(y = sde, colour = "SDE")) +
-  #geom_smooth(aes(y = eva, colour = "Free")) + 
-  #geom_smooth(aes(y = e200, colour = "E200"))
+  summarize(number = n()) %>% 
+  filter(number >= 40) %>% 
+  pull(firm)
 
+orig %>%
+  filter(Sys.Date() - date < 365) %>% 
+  filter(firm %in% good_firms) %>% 
+  ggplot(aes(x = date, y = vote, color = party)) + 
+  geom_smooth()
