7306a79b885b67df33f786b7c85d2de3e6fd8b8a
Shivani Aggarwal <saggarwal@college.harvard.edu>
2019-03-12 14:28:09 -0400

    Changed to working with Spain data; filtering for firms with more than 40 polls

--- script1.R
+++ script1.R
@@ -2,16 +2,28 @@ library(tidyverse)
 library(gt)
 library(janitor)
 
-orig <- read_csv(file = "polls_EE-parliament_2019-03-12.csv") %>% 
+orig <- read_csv(file = "https://pollofpolls.eu/get/polls/ES-parliament/format/csv") %>% 
   clean_names()
 
 orig %>% 
+  
+  #filter(date < as.Date("2018-03-12")) %>% 
+  
+  group_by(firm) %>% 
+  
+  summarize(numbers = n()) %>% 
+  
+  ungroup() %>% 
+  
+  filter(Sys.Date() - date < 365, !is.na(firm), numbers >= 40) %>% 
+  
   ggplot(aes(date)) + 
-  geom_smooth(aes(y = ekre, colour = "Ekre")) + 
-  geom_smooth(aes(y = green, colour = "Green")) +
-  geom_smooth(aes(y = irl, colour = "I")) + 
-  geom_smooth(aes(y = kesk, colour = "Centew")) +
-  geom_smooth(aes(y = ref, colour = "Reform")) + 
-  geom_smooth(aes(y = sde, colour = "SDE")) +
-  geom_smooth(aes(y = eva, colour = "???")) + 
-  geom_smooth(aes(y = e200, colour = "E200"))
+  geom_smooth(aes(y = psoe, colour = "Ekre")) + 
+  geom_smooth(aes(y = BNG, colour = "Green")) +
+  geom_smooth(aes(y = cc, colour = "I")) + 
+  geom_smooth(aes(y = erc, colour = "Centew")) +
+  geom_smooth(aes(y = pnveaj, colour = "Reform")) 
+  #geom_smooth(aes(y = sde, colour = "SDE")) +
+  #geom_smooth(aes(y = eva, colour = "Free")) + 
+  #geom_smooth(aes(y = e200, colour = "E200"))
+
